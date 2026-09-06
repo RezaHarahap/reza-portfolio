c126da252f2d9c822f0126992e0e95601634d42c
RezaHarahap <114418673+RezaHarahap@users.noreply.github.com>
2026-09-06 18:00:08 +0700

    feat: add professional credentials page

---
+++ client/src/pages/certifications/ProfessionalCertificationsPage.jsx
@@ -0,0 +1,10 @@
+import CategoryCertificationsPage from './CategoryCertificationsPage.jsx';
+
+export default function ProfessionalCertificationsPage() {
+  return (
+    <CategoryCertificationsPage
+      category="professional_credentials"
+      title="Professional Credentials"
+    />
+  );
+}
